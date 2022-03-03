e749803452353c903c68b56f8e5212480261fa40
Rayane <36335185+ohancee@users.noreply.github.com>
2022-03-03 18:23:08 +0100

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,1 +1,12 @@
 // Build a function that returns an array of integers from n to 1 where n>0.
+
+const reverseSeq = n => {
+  i = n
+  arr = []
+  do  {
+    arr.push(i);
+    i --;
+  }
+  while (i > 0);
+  return arr
+};
